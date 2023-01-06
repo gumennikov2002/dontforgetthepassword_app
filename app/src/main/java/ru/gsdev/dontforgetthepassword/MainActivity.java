67f2a7e492bf90ae27a40edb38937585bb9d87cd
sergey <gumennikov24@yandex.ru>
2023-01-06 19:11:01 +0400

    Design Refactor

--- app/src/main/java/ru/gsdev/dontforgetthepassword/MainActivity.java
+++ app/src/main/java/ru/gsdev/dontforgetthepassword/MainActivity.java
@@ -11,24 +11,27 @@ import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    //https://www.youtube.com/watch?v=suVgcrPwYKQ&t=0s
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView usercode = (TextView) findViewById(R.id.usercode);
+        TextView email = (TextView) findViewById(R.id.email);
+        TextView password = (TextView) findViewById(R.id.password);
         MaterialButton loginButton = (MaterialButton) findViewById(R.id.loginbtn);
-
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (usercode.getText().toString().equals("root")) {
-                    //correct
-                    return;
-                }
-
-                Toast.makeText(MainActivity.this, "Неверный код", Toast.LENGTH_SHORT).show();
-            }
-        });
+//
+//        loginButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (usercode.getText().toString().equals("root")) {
+//                    //correct
+//                    return;
+//                }
+//
+//                Toast.makeText(MainActivity.this, "Неверный код", Toast.LENGTH_SHORT).show();
+//            }
+//        });
     }
 }
